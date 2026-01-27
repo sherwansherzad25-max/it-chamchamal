4e6e45c1759befa312f877bad762d684f57b406b
sherwansherzad25-max <sherwan.sherzad25@gmail.com>
2026-01-27 10:18:07 -0800

    sw.js
    Add service worker for caching assets and handling fetch requests.

--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
-self.addEventListener('fetch', function(event) {
-    // ئەم کۆدە وادەکات سایتەکە ئامادە بێت بۆ ئینستاڵ بوون
+const CACHE_NAME = 'it-chamchamal-v1';
+const assets = [
+  './',
+  './index.html',
+  './it-icon-final-192.png',
+  './it-icon-final-512.png'
+];
+
+// دامەزراندنی سێرڤس وۆرکەر
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// وەڵامدانەوەی داواکارییەکان
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
