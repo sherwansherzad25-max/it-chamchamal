1cfb346d52921aee3fe9559f6878a637d4982aed
sherwansherzad25-max <sherwan.sherzad25@gmail.com>
2026-01-28 08:34:44 -0800

    Update service worker cache version and strategy
    Updated service worker to use new cache version and implement a network-first fetch strategy.

--- sw.js
+++ sw.js
@@ -1,25 +1,46 @@
-const CACHE_NAME = 'it-chamchamal-v1';
+const CACHE_NAME = 'it-chamchamal-v2'; // ناوی ڤێرژنەکەمان گۆڕی بۆ v2
 const assets = [
   './',
   './index.html',
   './it-icon-final-192.png',
-  './it-icon-final-512.png'
+  './it-icon-final-512.png',
+  './manifest.json'
 ];
 
-// دامەزراندنی سێرڤس وۆرکەر
+// دامەزراندن و سڕینەوەی کاشی کۆن
 self.addEventListener('install', event => {
+  self.skipWaiting(); // ڕێگە دەدات یەکسەر وەشانی نوێ جێگیر ببێت
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      cache.addAll(assets);
+      return cache.addAll(assets);
     })
   );
 });
 
-// وەڵامدانەوەی داواکارییەکان
+// چالاککردن و پاککردنەوەی ڤێرژنە کۆنەکان
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// ستراتیژی نوێ: سەرەتا ئینتەرنێت، ئەگەر نەبوو ئینجا کاش (Network First)
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then(res => {
+        // ئەگەر ئینتەرنێت هەبوو، وەشانی نوێ سەیڤ بکە
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, resClone);
+        });
+        return res;
+      })
+      .catch(() => caches.match(event.request)) // ئەگەر ئینتەرنێت نەبوو، وەشانی ناو مۆبایل نیشان بدە
   );
 });
